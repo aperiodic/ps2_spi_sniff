84a7dd198a1fc9525de5e2ad15bb46f9993639f3
Dan Lidral-Porter <dlp@aperiodic.org>
2013-05-07 04:05:10 -0700

    Rework storage buffers, first pass at syncing.
    There are now two ring buffer logs for SPI bytes, one for each side. If
    the buffers fill up, new SPI data will be dropped.
    
    Add a bunch of LED asserts in debug mode.
    
    First pass at a probably slow I2C syncing implementation using the
    Arduino Wire library.

--- spi_sniff.ino
+++ spi_sniff.ino
@@ -3,6 +3,10 @@
 
 #define DEBUG true
 
+#ifdef DEBUG
+  #define DEADLOCKED_LED 3
+#endif
+
 
 // Identity Arbitration
 // ====================
@@ -44,6 +48,8 @@ enum id our_id;
 // few paragraphs of that, up through "General packet format", and skim the
 // rest.
 //
+// [scanlime-notes]: https://gist.github.com/scanlime/5042071
+//
 // Every PS2 message begins with a fixed-length header, which includes the
 // number of data packets that will follow (note that this determination is even
 // a pure function of the message type and controller mode, so it can be
@@ -52,12 +58,12 @@ enum id our_id;
 // message by driving the controller's CS line high.
 
 enum ptcl_states {
-  IDLE,
-  HEADER_0,
-  HEADER_1,
-  HEADER_2,
-  DATA,
-  PENDING_TERMINATION
+  idle,
+  header_0,
+  header_1,
+  header_2,
+  data,
+  pending_termination
 };
 
 volatile enum ptcl_states ptcl_state;
@@ -66,6 +72,14 @@ volatile enum ptcl_states ptcl_state;
 
 // PS2 Dual Shock Controller State Machine
 // =======================================
+
+enum ctlr_states {
+  digital = 0x4,
+  analog = 0x7, // a.k.a. dual-shock mode
+  escape = 0xf
+}
+
+volatile enum ctlr_states ctlr_state;
 
 
 
@@ -79,31 +93,135 @@ volatile enum ptcl_states ptcl_state;
 // so I'm going to string together two Arduinos that both read one side of the
 // traffic, then communicate over an I2C bus to synchronize.
 //
-// As aspiring competent systems designers, we must ask ourselves what should
-// happen should I2C communication fail. We will attempt synchronization every
-// byte, but we'll keep a backlog of the last few bytes if we need them. Any
-// further bytes are logged to UART (and the condition to an LED) if in debug
-// mode (otherwise they are ignored).
+// We have two logs for storing the bytes, one for each side of the SPI traffic.
+// The bytes that we read go in our_log, and the bytes we get from the other
+// node over I2C go in their_log. When the logs fill up, they will be dumped to
+// UART after the next synchronization. If we get another SPI byte before we can
+// synchronize, then we just drop all the data in the logs, and if we're in
+// debug mode we'll light up an LED to indicate this.
 
-volatile byte IC2_SLACK 3
-volatile byte* ptcl_buff[IC2_SLACK];
+#define LOG_SIZE 128
+volatile byte* our_log[LOG_SIZE];
+volatile byte* their_log[LOG_SIZE];
+volatile byte bytes_unsynced;
+volatile byte our_log_head, our_log_tail, their_log_head, their_log_tail;
+
+// When a byte comes in over SPI, we check to see if there's room in our
+// ring buffer log for it. If so, we throw it in there and increment the count
+// of unsynchronized bytes. If there's not room, we just drop it on the floor
+// and if in debug mode we light up the SLOWSYNC_LED
 
 #ifdef DEBUG
-  volatile boolean had_i2c_backlog = false;
+  #define SLOWSYNC_LED 2
 #endif
 
-// After synchronization, the bytes are stored in a log buffer big enough to
-// contain all the bytes in the longest possible PS2 to controller message,
-// which is forty between the two sides according to [M. Elizabeth Scott's
-// protocol documentation] [scanlime-notes]. In debug mode, after each message
-// terminates, it is logged to UART.
-// [scanlime-notes]: https://gist.github.com/scanlime/5042071
+// SPI Interrupt Handler
+ISR (SPI_STC_vect) {
+  if (our_log_pos == LOG_SIZE) {
+    our_log_pos = 0;
+#ifdef DEBUG
+    digitalWrite(SLOWSYNC_LED, HIGH);
+#endif
+  }
+  our_log[our_log_pos++] = SPDR;
+  bytes_unsynced++;
+  if (bytes_unsynced > LOG_SIZE) {
+    bytes_unsynced = LOG_SIZE;
+  }
+}
 
-#define MAX_MSG_SIZE 40
-volatile byte* msg_log[MAX_MSG_SIZE];
-volatile int log_pos;
-volatile boolean log_full;
-int msgs_captured;
+// The synchronization process is pretty straightforward. The node with id alpha
+// acts as the I2C master, and first requests all the unsynchronized bytes from
+// the beta node, then transmits all its unsynchronized bytes back.
+
+void alpha_synchronize(void) {
+  // bail if we're not the alpha node
+  if (our_id != alpha) return;
+
+  byte bytes_synced = 0;
+  Wire.requestFrom(I2C_SLAVE_ADDRESS, bytes_unsynced);
+
+  // potential for deadlock here, if the two nodes receive different amounts of
+  // bytes over SPI, which shouldn't happen unless one of them gets disconnected
+  // from the SPI bus
+#ifdef DEBUG
+  digitalWrite(DEADLOCKED_LED, HIGH);
+#endif
+  while (Wire.available() < bytes_unsynced) {}
+
+  while (Wire.available() > 0) {
+    // if there's room in the log chuck it in
+    if (their_log_head != (their_log_tail + 1) % LOG_SIZE) {
+      their_log[their_log_tail++] = Wire.read();
+      their_log_tail %= LOG_SIZE;
+    } else {
+      // if the ring buffer is full we just drop the data on the floor
+      Wire.read();
+#ifdef DEBUG
+      digitalWrite(SLOWDUMP_LED, HIGH);
+#endif
+    }
+    bytes_synced++;
+  }
+
+#ifdef DEBUG
+  digitalWrite(DEADLOCKED_LED, LOW);
+#endif
+
+  // send our bytes
+  byte uhead = unsynced_head(bytes_unsynced, our_log_tail, LOG_SIZE);
+  Wire.beginTransmission(I2C_SLAVE_ADDRESS);
+  for (byte i = 0; i < bytes_unsynced; i++) {
+    Wire.write(our_log[(uhead + i) % LOG_SIZE]);
+  }
+  Wire.endTransmission();
+#ifdef DEBUG
+  if (bytes_unsynced != bytes_synced) {
+    digitalWrite(UNSYNCHRONIZED_LED, HIGH);
+  }
+#endif
+  bytes_unsynced -= bytes_synced;
+}
+
+// The beta node acts as the I2C slave. First it must handle the master's
+// request for all of its unsynchronized bytes.
+
+void beta_synchronize_tx(void) {
+  byte uhead = unsynced_head(bytes_unsynced, our_log_tail, LOG_SIZE);
+  for (byte i = 0; i < bytes_unsynced, i++) {
+    Wire.write(our_log[(uhead + i) % LOG_SIZE]);
+  }
+}
+
+// Then it must receive all of the master's unsynchronized bytes.
+
+void beta_synchronize_rx(int bytes_synced) {
+  while (Wire.available() > 0) {
+    if (their_log_head != (their_log_tail + 1) % LOG_SIZE) {
+      their_log[their_log_tail++] = Wire.read();
+      their_log_tail %= LOG_SIZE;
+    } else {
+      Wire.read();
+#ifdef DEBUG
+      digitalWrite(SLOWDUMP_LED, HIGH);
+#endif
+    }
+  }
+
+#ifdef DEBUG
+  if (bytes_unsynced != bytes_synced) {
+    digitalWrite(UNSYNCHRONIZED_LED, HIGH);
+  }
+#endif
+  bytes_unsynced -= bytes_synced;
+}
+
+byte unsynced_head(byte unsynced, byte log_pos, byte log_size) {
+  // log_pos is where next byte will end up, hence the -1. The '+ 2*log_size
+  // % log_size' ensures that 0 <= start < log_size.
+  return ((log_pos - unsynced - 1) + 2*log_size) % log_size;
+}
+
 
 
 void setup(void) {
@@ -132,19 +250,6 @@ void setup(void) {
   SPCR |= 1 << DORD; // data is transmitted in LSB order
   SPCR |= 1 << SPIE; // enable SPI interrupts
   SPCR |= 1 << SPE; // enable SPI
-}
-
-// SPI Interrupt Handler
-ISR (SPI_STC_vect) {
-  if (pos < buff_size) {
-    if (are_master) {
-      master_buff[pos++] = SPDR;
-    } else {
-      slave_buff[pos++] = SPDR;
-    }
-  } else {
-    buffer_full = true;
-  }
 }
 
 // I2C Master Received Interrupt
